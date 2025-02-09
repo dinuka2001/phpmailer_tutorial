f73095021b8c8e8c2cc19ab0eb993bbd903b5ffa
dinuka2001 <2022cs047@stu.ucsc.cmb.ac.lk>
2025-02-10 04:03:39 +0530

    update code

--- src/Mailer.php
+++ src/Mailer.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 // include phpmailer library files
-require 'vendor/autoload.php';
+// require 'vendor/autoload.php';
 
 // create instence of phpmailer 
 $mail = new PHPMailer(true); //with error exeption ennabled
@@ -16,16 +16,16 @@ try{
     $mail->isSMTP(); //set mailer to use smtp
     $mail->Host = 'smtp.gmail.com'; //specify main and backup server
     $mail->SMTPAuth = true; //enable smtp authentication
-    $mail->Username = 'dinukasahan2001@gmail.com'; //smtp username
-    $mail->Password = 'iwqh plcq mpgg yuqv'; // smtp password that is google app password
+    $mail->Username = 'server@mail.com'; //smtp username
+    $mail->Password = 'app pass word'; // smtp password that is google app password
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
     // recipients
     // sender (server mail)
-    $mail->setFrom('dinukasahan2001@gmail.com', 'dinuka');
+    $mail->setFrom('server@mail.com', 'server');
     // receiver (client mail)
-    $mail->addAddress('dinukasahan2020@gmial.com', "sahan");
+    $mail->addAddress('client@mial.com', "client");
 
 
     // email content 
